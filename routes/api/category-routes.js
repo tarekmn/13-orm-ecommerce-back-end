d8c69dc3719d48349410bed58a6c96a74cae945d
tarekmn <nytarek@gmail.com>
2022-10-21 15:30:33 -0500

    creating models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,6 +4,16 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const data = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+  
   // find all categories
   // be sure to include its associated Products
 });
